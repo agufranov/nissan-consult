a114cdc1227a8c96afa5d73204478509ff9c005a
agufranov <al.gufranov@gmail.com>
2018-06-15 18:41:59 +0300

    Utils

--- src/consult/util.ts
+++ src/consult/util.ts
@@ -1,2 +1,10 @@
-// tslint:disable-next-line:no-magic-numbers
+// tslint:disable:no-magic-numbers
 export const invert: ((byte: number) => number) = (byte: number): number => 0xFF - byte
+
+export const toHex: ((n: number) => string) = (n: number): string => `0${n.toString(16)}`.slice(-2)
+
+export const aToHex: ((ns: number[]) => string) = (ns: number[]): string => ns.map(toHex).join(' ')
+
+export const fromHex: ((n: string) => number) = (n: string): number => parseInt(n, 16)
+
+export const aFromHex: ((ns: string) => number[]) = (ns: string): number[] => ns.split(' ').map(fromHex)
